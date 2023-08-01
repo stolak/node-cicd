7f8d05ac6be431fd99d0b6e5c69a15f1b6c6e921
Stephen <stolak>
2023-08-01 13:27:34 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express();
+const path = require('path')
+
+app.get('/names', (req, res) => {
+  res.send('Akinbobola Olawole');
+})
+
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+})
