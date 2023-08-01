b9dc190b9df8b12e9493c41edf6a432956512506
Stephen <stolak>
2023-08-01 13:59:33 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path')
 
 app.get('/names', (req, res) => {
-  res.send('Akinbobola Olawole stephen');
+  res.send('Akinbobola Olawole stephenh');
 })
 
 app.listen(5000, () => {
